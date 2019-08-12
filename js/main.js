7d968efe4f1f7e1140bcbf3e406258b7cecc5f67
Sam Brown <sammybrrr@gmail.com>
2019-08-12 08:22:36 +0200

    Mailto

--- js/main.js
+++ js/main.js
@@ -128,4 +128,49 @@ $(document).ready(function(){
           $('#mc_embed_signup').find('form').ajaxChimp();
       });   
 
+      // Get the form.
+    var form = $('#myForm');
+
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+    // Set up an event listener for the contact form.
+    $(form).submit(function(event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
+        // Serialize the form data.
+        var formData = $(form).serialize();
+
+    // Submit the form using AJAX.
+        $.ajax({
+            type: 'POST',
+            url: 'https://nd-mailer-service.herokuapp.com/mail/sambk-website',
+            data: formData
+        })
+        .done(function(response) {
+            // Make sure that the formMessages div has the 'success' class.
+            $(formMessages).removeClass('error');
+            $(formMessages).addClass('success');
+        
+            // Set the message text.
+            $(formMessages).text('Thank you for getting in touch. I will get back to you as soon as possible.');
+        
+            // Clear the form.
+            $(form).html('')
+        })
+        
+        .fail(function(data) {
+            // Make sure that the formMessages div has the 'error' class.
+            $(formMessages).removeClass('success');
+            $(formMessages).addClass('error');
+        
+            // Set the message text.
+            if (data.responseText !== '') {
+                $(formMessages).text(data.responseText);
+            } else {
+                $(formMessages).text('Oops! An error occured and your message could not be sent.');
+            }
+        });
+        
+        // TODO
+    });
  });
